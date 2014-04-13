3d0516e15cc658551a63457a142d5116edb9ca10
Vincent Heuken <vincent@vincentheuken.com>
2014-04-13 15:36:53 -0700

    Fixed keyboard input

--- src/sdl/sdlinput.cpp
+++ src/sdl/sdlinput.cpp
@@ -6,11 +6,11 @@
  * /______/ //______/ //_/ //_____/\ /_/ //_/ //_/ //_/ //_/ /|_/ /
  * \______\/ \______\/ \_\/ \_____\/ \_\/ \_\/ \_\/ \_\/ \_\/ \_\/
  *
- * Copyright (c) 2004, 2005, 2006, 2007 Olof Naessén and Per Larsson
+ * Copyright (c) 2004, 2005, 2006, 2007 Olof Naessï¿½n and Per Larsson
  *
  *                                                         Js_./
  * Per Larsson a.k.a finalman                          _RqZ{a<^_aa
- * Olof Naessén a.k.a jansem/yakslem                _asww7!uY`>  )\a//
+ * Olof Naessï¿½n a.k.a jansem/yakslem                _asww7!uY`>  )\a//
  *                                                 _Qhm`] _f "'c  1!5m
  * Visit: http://guichan.darkbits.org             )Qk<P ` _: :+' .'  "{[
  *                                               .)j(] .d_/ '-(  P .   S
@@ -261,10 +261,7 @@ namespace gcn
               value = Key::PAUSE;
               break;
           case SDLK_SPACE:
-              if (event.type == SDL_KEYUP)
-              {
-                  value = Key::SPACE;
-              }
+              value = Key::SPACE;
               break;
           case SDLK_ESCAPE:
               value = Key::ESCAPE;
@@ -373,6 +370,7 @@ namespace gcn
               break;
 
           default:
+              value = keysym.sym;
               break;
         }
 
