00e2808ff2f0c92e4dba4dbc4cae18eadd28cbbd
Gwilherm Baudic <gbaudic@users.noreply.github.com>
2017-11-20 17:23:46 +0100

    Fixing catch block issue

--- src/widgets/messagebox.cpp
+++ src/widgets/messagebox.cpp
@@ -6,11 +6,11 @@
  * /______/ //______/ //_/ //_____/\ /_/ //_/ //_/ //_/ //_/ /|_/ /
  * \______\/ \______\/ \_\/ \_____\/ \_\/ \_\/ \_\/ \_\/ \_\/ \_\/
  *
- * Copyright (c) 2004, 2005, 2006, 2007 Olof Naessén and Per Larsson
+ * Copyright (c) 2004, 2005, 2006, 2007 Olof NaessÃ©n and Per Larsson
  * Copyright (c) 2017 Gwilherm Baudic
  *                                                         Js_./
  * Per Larsson a.k.a finalman                          _RqZ{a<^_aa
- * Olof Naessén a.k.a jansem/yakslem                _asww7!uY`>  )\a//
+ * Olof NaessÃ©n a.k.a jansem/yakslem                _asww7!uY`>  )\a//
  *                                                 _Qhm`] _f "'c  1!5m
  * Visit: http://guichan.darkbits.org             )Qk<P ` _: :+' .'  "{[
  *                                               .)j(] .d_/ '-(  P .   S
@@ -100,7 +100,7 @@ namespace gcn
         {
         	requestModalFocus();
         } 
-        catch () 
+        catch (Exception e) 
         {
         	// Not having modal focus is not critical
         }
@@ -162,7 +162,7 @@ namespace gcn
         {
         	requestModalFocus();
         } 
-        catch () 
+        catch (Exception e) 
         {
         	// Not having modal focus is not critical
         }
