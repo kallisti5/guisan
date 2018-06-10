d7e02fba4709c2093712cc11141de65321841fc1
gbaudic <gbaudic@users.noreply.github.com>
2018-06-10 16:25:59 +0200

    Adding the included demo as a project in solution Some MSVC-specific compilation fixes

--- demo/ff/src/ffcontainer.cpp
+++ demo/ff/src/ffcontainer.cpp
@@ -43,7 +43,11 @@
 
 #include "ffcontainer.hpp"
 #include <cmath>
+#ifndef _MSC_VER
 #include <SDL2/SDL.h>
+#else
+#include <SDL.h>
+#endif // !_MSC_
 
 int FFContainer::mInstances = 0;
 gcn::Image *FFContainer::mCornerUL = 0;
