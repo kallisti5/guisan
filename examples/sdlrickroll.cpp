be378a2621e27c48e5e39f4e2a80af46c0843e1c
Gwilherm Baudic <gbaudic@users.noreply.github.com>
2019-01-23 21:17:17 +0100

    Adding new widget InputBox

--- examples/sdlrickroll.cpp
+++ examples/sdlrickroll.cpp
@@ -34,9 +34,12 @@ gcn::SDLImageLoader* imageLoader; // For loading images
 gcn::Gui* gui;            // A Gui object - binds it all together
 gcn::Container* top;      // A top container
 gcn::ImageFont* font;     // A font
-gcn::Button* button1;     // A button for actionlistening
+gcn::Button* button1;     // A button for actionlistening (triggers msgbox)
 gcn::Label* label1;       // And a label for button1
+gcn::Button* button2;     // Button for InputBox
+gcn::Label* label2;       // Label for inputbox
 gcn::MessageBox* msgBox = NULL;
+gcn::InputBox* inputBox = NULL;
 
 /*
  * To be able to recieve actions we must have a class inheriting from
@@ -73,6 +76,31 @@ public:
 			msgBox->setVisible(true);
 			msgBox->addActionListener(this);
 			top->add(msgBox, 270, 180);
+		}
+		else if (actionEvent.getSource() == button2)
+		{
+			if(inputBox)
+			{
+				delete inputBox;
+				inputBox = NULL;
+			}
+			inputBox = new gcn::InputBox("Name request", "Enter your name below", "Here you go", "No way");
+			inputBox->setVisible(true);
+			inputBox->addActionListener(this);
+			top->add(inputBox, 270, 180);
+		}
+		else if (actionEvent.getSource() == inputBox)
+		{
+			inputBox->setVisible(false);
+			if(inputBox->getClickedButton() == 1)
+			{
+				label2->setCaption("No name");
+			}
+			else
+			{
+				label2->setCaption("Your name is " + inputBox->getText());
+			}
+			label2->adjustSize();
 		}
 	}
 };
@@ -131,27 +159,34 @@ void init()
 
 	// Create buttons
 	button1 = new gcn::Button("Click me");
+	button2 = new gcn::Button("Enter your name");
 	// Set the buttons position
 	button1->setPosition(120, 230);
+	button2->setPosition(420, 230);
 	// Add the buttons to the top container
 	top->add(button1);
+	top->add(button2);
 
 	// Create labels
 	label1 = new gcn::Label("Get rick rolled");
+	label2 = new gcn::Label("What is your name");
 	// Set the labels position
 	label1->setPosition(100, 200);
+	label2->setPosition(400, 200);
 	// Add the labels to the top container
 	top->add(label1);
-
+	top->add(label2);
 
 	// Set the buttons action event id's.
 	button1->setActionEventId("button1");
+	button2->setActionEventId("button2");
 
 	// Make an instance of the ButtonActionListener
 	buttonActionListener = new ButtonActionListener();
 
 	// Add the ButtonActionListener to the buttons action listeners
 	button1->addActionListener(buttonActionListener);
+	button2->addActionListener(buttonActionListener);
 }
 
 /**
@@ -171,8 +206,14 @@ void halt()
 	{
 		delete msgBox;
 	}
+	if(inputBox != NULL)
+	{
+		delete inputBox;
+	}
 	delete label1;
 	delete button1;
+	delete label2;
+	delete button2;
 	delete font;
 	delete top;
 	delete gui;
