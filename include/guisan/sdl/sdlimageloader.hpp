919e76ff4ab2837c35819ab634d1a3ef235984ca
Dimitris Panokostas <midwan@gmail.com>
2023-10-31 20:36:51 +0100

    Fixed memory leak in SDL Image Loader
    The loaded image was never released

--- include/guisan/sdl/sdlimageloader.hpp
+++ include/guisan/sdl/sdlimageloader.hpp
@@ -6,11 +6,11 @@
  * /______/ //______/ //_/ //_____/\ /_/ //_/ //_/ //_/ //_/ /|_/ /
  * \______\/ \______\/ \_\/ \_____\/ \_\/ \_\/ \_\/ \_\/ \_\/ \_\/
  *
- * Copyright (c) 2004, 2005, 2006, 2007 Olof Naessén and Per Larsson
+ * Copyright (c) 2004, 2005, 2006, 2007 Olof Naessï¿½n and Per Larsson
  *
  *                                                         Js_./
  * Per Larsson a.k.a finalman                          _RqZ{a<^_aa
- * Olof Naessén a.k.a jansem/yakslem                _asww7!uY`>  )\a//
+ * Olof Naessï¿½n a.k.a jansem/yakslem                _asww7!uY`>  )\a//
  *                                                 _Qhm`] _f "'c  1!5m
  * Visit: http://guichan.darkbits.org             )Qk<P ` _: :+' .'  "{[
  *                                               .)j(] .d_/ '-(  P .   S
@@ -77,12 +77,14 @@ namespace gcn
         // Inherited from ImageLoader
 
         virtual Image* load(const std::string& filename, bool convertToDisplayFormat = true);
+        virtual void free();
 
     protected:
         virtual SDL_Surface* loadSDLSurface(const std::string& filename);
         virtual SDL_Texture* loadSDLTexture(const std::string& filename);
         virtual SDL_Surface* convertToStandardFormat(SDL_Surface* surface);
         SDL_Renderer* mRenderer;
+        Image* mImage;
     };
 }
 
