3d97c64ebe405f4c3686abe96180356cd78e951c
Dimitris Panokostas <midwan@gmail.com>
2023-12-30 00:31:07 +0100

    Refactor: window should check the font size for the title bar, instead of a hard coded value

--- src/widgets/window.cpp
+++ src/widgets/window.cpp
@@ -85,7 +85,7 @@ namespace gcn
         setCaption(caption);
         setBorderSize(1);
         setPadding(2);
-        setTitleBarHeight(16);
+        setTitleBarHeight(getFont()->getHeight() + 2);
         setAlignment(Graphics::CENTER);
         addMouseListener(this);
         setMovable(true);
